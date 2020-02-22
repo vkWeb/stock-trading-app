6403894f14712100d43a305ff7d808b0bc87a483
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2020-02-22 07:16:29 +0530

    fix: test if session fixed

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ class Config(object):
     """All the configurations required to run the web application"""
 
     # Set SECRET_KEY to sign cookies and for other security related tools
-    # SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
     # Turn on Jinja template reloading
-    # TEMPLATES_AUTO_RELOAD = True
+    TEMPLATES_AUTO_RELOAD = True
 
     # Configure session to use filesystem (instead of signed cookies)
     SESSION_FILE_DIR = tempfile.mkdtemp()
