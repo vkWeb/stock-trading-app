9215b2d398f7a6f91de2439d204d77bf0cc7107b
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2020-02-22 21:33:43 +0530

    fix: experiment

--- finance/routes.py
+++ finance/routes.py
@@ -18,6 +18,8 @@ flask_session.Session(app)
 @app.after_request
 def after_request(response):
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
     return response
 
 
